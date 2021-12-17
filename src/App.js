1f2bb7b85d14c86ecf32c5413e28ce58e57d73b8
gina3-uk <84281092+gina3-uk@users.noreply.github.com>
2021-12-17 17:36:52 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,35 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>React Weather App</h1>
       <Search />
+      <footer>
+        This project was created by Gina Lewis. Open source code available on{" "}
+        <a
+          href="https://github.com/gina3-uk/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+        <br />
+        <a
+          href="https://www.linkedin.com/in/gina-lewis-98a96476/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <br />
+        <a
+          href="https://www.instagram.com/gina.lewis/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+      </footer>
     </div>
   );
 }
